a0de3dd96db087edb4054e5179720984e7f433ba
Tushar Kumar Ladia <tusharladia03@gmail.com>
2025-03-29 01:17:02 +0530

    Update App.jsx

--- src/freelance-reputation-frontend/src/App.jsx
+++ src/freelance-reputation-frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./pages/Home.jsx";
+import { AuthProvider } from "./pages/useAuthClient.jsx";
+import Login from "./pages/login.jsx";
 
 function App() {
   return (
     <div>
+      <AuthProvider>
       <Home />
+      <Login />
+      </AuthProvider>
     </div>
   );
 }
